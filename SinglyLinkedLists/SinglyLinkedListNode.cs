8b9d3c776da02422a6241dd4d5e1f7d0413a47bf
BrendonPierson <brendonpierson@gmail.com>
2015-10-26 15:58:42 -0500

    First milestone complete.

--- SinglyLinkedLists/SinglyLinkedListNode.cs
+++ SinglyLinkedLists/SinglyLinkedListNode.cs
@@ -15,14 +15,14 @@ namespace SinglyLinkedLists
         private SinglyLinkedListNode next;
         public SinglyLinkedListNode Next
         {
-            get { throw new NotImplementedException(); }
-            set { throw new NotImplementedException(); }
+            get { return next; }
+            set { if (value != this) { next = value; } else { throw new ArgumentException(); }  }
         }
 
         private string value;
         public string Value 
         {
-            get { throw new NotImplementedException(); }
+            get { return value; }
         }
 
         public static bool operator <(SinglyLinkedListNode node1, SinglyLinkedListNode node2)
@@ -39,10 +39,15 @@ namespace SinglyLinkedLists
 
         public SinglyLinkedListNode(string value)
         {
-            throw new NotImplementedException();
-
+            this.value = value;
+            this.next = null;
             // Used by the visualizer:
             allNodes.Add(this);
+        }
+
+        public override string ToString()
+        {
+            return this.value.ToString();
         }
 
         // READ: http://msdn.microsoft.com/en-us/library/system.icomparable.compareto.aspx
